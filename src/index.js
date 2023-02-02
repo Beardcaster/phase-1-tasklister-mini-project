13fd11f46c4686982eaba7a7281632b60c86c42e
jayson ambrose <jayson.ambrose.87@gmail.com>
2023-02-02 11:51:34 -0700

    core deliverables achieved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskField = document.getElementById("new-task-description");
+  const taskList = document.querySelector('#tasks');
+  taskField.id = "fieldContent";
+  
+
+  document.querySelector('form').addEventListener('submit', (e) => {
+
+    if(taskField.value.length >= 1){   //error handling 
+
+    e.preventDefault();
+
+    const newItem = e.target.fieldContent.value;
+
+    const li = document.createElement('li');
+    li.innerText = newItem;
+    li.setAttribute("class", "userEntry");    
+    taskList.append(li);
+
+    // const btn = document.createElement('button');
+    // taskList.userEntry.appendChild(btn);
+
+    }else {                     //
+      e.preventDefault();       //error handling
+      console.log('error');}    //
 });
+
+})
